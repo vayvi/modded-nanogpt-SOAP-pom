89f655eedae861b64b44c65d6d041a38c83c30f6
David Picard <picard@ensea.fr>
2025-03-30 12:40:41 +0200

    - change all optimizers parameters

--- train_gpt2.py
+++ train_gpt2.py
@@ -238,11 +238,10 @@ class GPT(nn.Module):
 
     def configure_optimizers(self, weight_decay, learning_rate, betas):
         optimizer = CombinedOptimizer([
-            torch.optim.AdamW(self.lm_head.parameters(), lr=learning_rate, betas=betas, weight_decay=0),
+            torch.optim.AdamW(self.lm_head.parameters(), lr=learning_rate, weight_decay=weight_decay),
             # SOAP(self.transformer.h.parameters(), lr=learning_rate, betas=(.95, .95), weight_decay=0, precondition_frequency=10)
-            #OrthogonalNesterov(self.transformer.h.parameters(), lr=10 * learning_rate, momentum=0.95)
-            torch.optim.AdamW(self.transformer.h.parameters(), lr=learning_rate, weight_decay=weight_decay, eps=1e-10,
-                              fused=True)
+            # OrthogonalNesterov(self.transformer.h.parameters(), lr=10 * learning_rate, momentum=0.95)
+            torch.optim.AdamW(self.transformer.h.parameters(), lr=learning_rate, weight_decay=weight_decay)
         ])
         return optimizer
 
