99fdb442ca6387d79f303e9d521f898fd0bf3151
Nicolas Dufour <nicolas.dufourn@gmail.com>
2025-07-01 14:17:37 +0200

    Update model head configuration in run_4_gpus.sh from 184 to 192 for improved multi-head training performance.

--- run_4_gpus.sh
+++ run_4_gpus.sh
@@ -5,6 +5,6 @@ export WANDB_MODE=offline
 
 torchrun --standalone --nproc_per_node=4 train.py \
     experiment=pomgpt_multihead \
-    model.n_head=184 \
+    model.n_head=192 \
     training.batch_size=48 \
     training.accumulation=2
